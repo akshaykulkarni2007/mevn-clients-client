549d782d4cc4a9139f42fd498149cf7b65ff4db5
Akshay Kulkarni <akshaykulkarni2007@gmail.com>
2022-12-05 19:44:52 +0530

    env fix

--- src/Utils/axios.js
+++ src/Utils/axios.js
@@ -1,8 +1,5 @@
 import axios from 'axios'
 
-const baseURL = process.env.VUE_APP_API_BASE_URL
-console.log(process.env.VUE_APP_API_BASE_URL, baseURL)
-
 export const api = axios.create({
-	baseURL,
+	baseURL: process.env.VUE_APP_API_BASE_URL,
 })
